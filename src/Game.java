dee4e7fed979fb5539c9e533c67030edda98add8
elllizzzzza <egalstyan@griddynamics.com>
2025-09-23 17:27:04 +0400

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -54,7 +54,7 @@ public class Game {
         for(int i = 0; i < 3; i++){
             System.out.print("| ");
             for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + ' ');
+                System.out.print(board[i][j] + " ");
             }
             System.out.println("| ");
         }
